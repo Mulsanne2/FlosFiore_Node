1c558c5f02414748d670c7cbafc7fed9a5e4d75d
sujin11-kim <su091876@gmail.com>
2022-08-22 23:06:43 +0900

    비번 틀린경우 return 변경

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 const {logger} = require('./winston');
 
-// TODO: 본인의 DB 계정 입력
+
 const pool = mysql.createPool({
     host: 'flower.cfeyrpadoh9b.ap-northeast-2.rds.amazonaws.com',
     user: 'admin',
